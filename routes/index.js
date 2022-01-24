8a4cf840655061002486850b5083877f777b0b07
Gaston <gastonbrunijavier@hotmail.com>
2022-01-24 14:20:44 -0300

    Agregamos carpeta views utilizando Pug.

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,6 @@ const proyectosController = require('../controllers/proyectosController');
 module.exports = function(){
     // definimos rutas para el home
     router.get('/', proyectosController.proyectosHome);
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros')
-    });
     return router;
 }
 
