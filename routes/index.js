6974f3f9954f4225df1d128ed1fb82aef5919b1b
Gaston <gastonbrunijavier@hotmail.com>
2022-01-24 16:00:13 -0300

    Agregamos opcion de agregar proyecto nuevo.

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 // importamos el controlador
 const proyectosController = require('../controllers/proyectosController');
 
-
 module.exports = function(){
     // definimos rutas para el home
     router.get('/', proyectosController.proyectosHome);
+    router.get('/nuevo-proyecto', proyectosController.formularioProyecto)
     return router;
 }
 
