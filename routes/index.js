f9db0e9f624cdd9b1f4afff535e622259a8b4497
Gaston <gastonbrunijavier@hotmail.com>
2022-01-24 16:33:18 -0300

    Agregamos metodo POST para la carga de datos.
    Se recomienda utilizar el metodo POST para que no tome por default el GET y no queden pegados los datos que agregue el usuario.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ module.exports = function(){
     // definimos rutas para el home
     router.get('/', proyectosController.proyectosHome);
     router.get('/nuevo-proyecto', proyectosController.formularioProyecto)
+    router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
     return router;
 }
+
+
 
