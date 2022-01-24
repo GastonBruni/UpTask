1a38f18558d0617a4b160976ffaf21817789a51c
Gaston <gastonbrunijavier@hotmail.com>
2022-01-24 13:00:39 -0300

    Utilizamos patron de diseño MVC (Model - View - Controller)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// importamos el controlador
+const proyectosController = require('../controllers/proyectosController');
+
+
 module.exports = function(){
     // definimos rutas para el home
-    router.get('/', (req, res) => {
-        res.send('Index')
-    });
+    router.get('/', proyectosController.proyectosHome);
     router.get('/nosotros', (req, res) => {
         res.send('Nosotros')
     });
