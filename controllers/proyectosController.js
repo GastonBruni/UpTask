9c6501ae4ac005056e16e957cea92c49fcb6c8d5
Gaston <Gastonbrunijavier@gmail.com>
2022-01-28 11:46:41 -0300

    Agregamos opcion de modificar titulo, ademas instalamos mas dependecinas.

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -9,13 +9,18 @@ exports.proyectosHome = async (req, res) => {
     });
 }
 
-exports.formularioProyecto = (req, res) => {
+exports.formularioProyecto = async (req, res) => {
+    const proyectos = await Proyectos.findAll();
+
     res.render('nuevoProyecto', {
-        nombrePagina: 'Nuevo Proyecto'
+        nombrePagina: 'Nuevo Proyecto',
+        proyectos
     })
 }
 
-exports.nuevoProyecto = (req, res) => {
+exports.nuevoProyecto = async (req, res) => {
+    const proyectos = await Proyectos.findAll();
+
     // enviar a la consola lo que el usuario escriba.
     // console.log(req.body);
 
@@ -32,12 +37,87 @@ exports.nuevoProyecto = (req, res) => {
     if(errores.length > 0){
         res.render('nuevoProyecto', {
             nombrePagina: 'Nuevo Proyecto',
-            errores
+            errores,
+            proyectos
         })
     }else{
         // no hay errores
         // insertamos en la BD.
-        Proyectos.create({ nombre});
+        await Proyectos.create({ nombre});
+        res.redirect('/');
+    }
+}
+
+exports.proyectoPorUrl = async (req, res, next) => {
+    const proyectosPromise =  Proyectos.findAll();
+
+    const proyectoPromise = await Proyectos.findOne({
+        where: {
+            url: req.params.url
+        }
+    });
+
+    const[proyectos, proyecto] = await Promise.all([proyectosPromise, proyectoPromise]);
+
+    if(!proyecto) return next();
+
+    // render a la vista
+    res.render('tareas', {
+        nombrePagina: 'Tareas de Proyecto',
+        proyecto,
+        proyectos
+    })
+
+}
+
+exports.formularioEditar = async (req, res) => {
+    const proyectosPromise =  Proyectos.findAll();
+
+    const proyectoPromise = await Proyectos.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    const[proyectos, proyecto] = await Promise.all([proyectosPromise, proyectoPromise]);
+
+    // render a la vista
+    res.render('nuevoProyecto', {
+        nombrePagina: 'Editar Proyecto',
+        proyectos,
+        proyecto
+    })
+}
+
+exports.ActualizarProyecto = async (req, res) => {
+    const proyectos = await Proyectos.findAll();
+
+    // enviar a la consola lo que el usuario escriba.
+    // console.log(req.body);
+
+    // validamos que tengamos algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if(!nombre){
+        errores.push({'texto': 'Agrega un Nombre al Proyecto'})
+    }
+
+    // si hay errores
+    if(errores.length > 0){
+        res.render('nuevoProyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores,
+            proyectos
+        })
+    }else{
+        // no hay errores
+        // insertamos en la BD.
+        await Proyectos.update(
+            { nombre: nombre},
+            { where: {id: req.params.id}}
+            );
         res.redirect('/');
     }
 }
--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// importar helpers con algunas funciones
+const helpers = require('./helpers');
+
 // crear conexion a la BD
 const db = require('./config/db');
+const res = require('express/lib/response');
 
 // importar el modelo
 require('./models/Proyectos');
@@ -14,18 +18,23 @@ db.sync();
 // crear una app de express
 const app = express();
     
-
 // donde cargar los archivos estaticos
 app.use(express.static('public'));
 
 // habilitamos Pug
 app.set('view engine', 'pug');
 
-// habilitamos bodyParser para leer datos del formulario
-app.use(bodyParser.urlencoded({extended: true}));
-
 // añadir la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
+
+// pasar var dump a la aplicacion (Middleware)
+app.use((req, res, next) => {
+    res.locals.vardump = helpers.vardump;
+    next();
+});
+
+// habilitamos bodyParser para leer datos del formulario
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', routes());
 
