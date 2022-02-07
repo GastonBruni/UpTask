f23504489751ac5689ba2221bb268489928e991d
Gaston <Gastonbrunijavier@gmail.com>
2022-02-07 11:12:21 -0300

    Agregamos variables de entorno.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize');
+// importar valores de variables.env
+require('dotenv').config({path: 'variables.env'});
 
-const db = new Sequelize('uptasknode', 'root', 'Juarezclaudia10', {
-  host: '127.0.0.1',
+const db = new Sequelize(
+  process.env.BD_NOMBRE, 
+  process.env.BD_USER, 
+  process.env.BD_PASS, {
+  host: process.env.BD_HOST,
   dialect: 'mysql',
-  port: '3306',
+  port: process.env.BD_PORT,
   define: {
       timestamps: false
   },
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('./config/passport');
 
+// importar valores de variables.env
+require('dotenv').config({path: 'variables.env'});
+
 // importar helpers con algunas funciones
 const helpers = require('./helpers');
 
@@ -61,4 +64,10 @@ app.use((req, res, next) => {
 
 app.use('/', routes());
 
-app.listen(3000);
+// Servidor y Puerto
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port,host, () => {
+    console.log('El servidor esta funcionando');
+})
